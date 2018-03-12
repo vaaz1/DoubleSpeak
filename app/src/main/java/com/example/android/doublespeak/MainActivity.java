b52113828cd264135072c43f5d1dec5103b4241a
Roman Smirnov <roman.smirnv@gmail.com>
2018-03-12 11:16:49 +0200

    added lottie animation library. added glide image loading library to load images and gifs. added cardview design support library. added palceholder sounds as raw resources. added internet permission to manifest.

--- app/src/main/java/com/example/android/doublespeak/MainActivity.java
+++ app/src/main/java/com/example/android/doublespeak/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.doublespeak;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView cardImg = findViewById(R.id.card_image);
+
+        Glide.with(this).load().into(cardImg);
     }
 }
