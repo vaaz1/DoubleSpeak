07a254233b69ff795ac390fc68ea3c51f1c48cff
Vera <albiezvera@gmail.com>
2018-03-16 06:27:09 +0200

    card_item layout activity_win screen

---
+++ app/src/main/java/com/example/android/doublespeak/activities/GameFinishActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.doublespeak.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.doublespeak.R;
+
+/**
+ * Created by Vera on 16.03.2018.
+ */
+
+public class GameFinishActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win);
+    }
+}
